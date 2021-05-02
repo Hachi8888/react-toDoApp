f76dca8d9598d57f1e77752aff7fc3452a50a2d6
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-02 23:20:59 +0900

    [add] validation of the "InputTodo"

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>※登録できるのは5個までです。</p>
+      )}
 
       <IncompleteTodos
         todos={incompleteTodos}
